5167017be104145971a347b995fc1c3f8bbe746a
Godfrey1234 <godfrey555mabena@gmail.com>
2022-02-07 05:50:24 -0800

    updates

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<link  rel="stylesheet" type= "text/css" href="styles.css">	
+    <head>
+      <nav>
+		<img src="images/cybernet1.jpeg" id="logo">
+		<ul>
+			<li><a href="#welcome">Home</a></li>
+			<li><a href="#serve">Services</a></li>
+			<li><a href="#">Directions</a></li>
+			<li><a href="#fot">Contact Us</a></li>
+      </ul>
+	  </nav>
+ 	</head>
+
+<body>
+	<div id = "content">
+		
+		<div id= "row">
+			
+			<div class="content2">
+				<div id="content-text">
+				<div id="wrapper">
+    				<div id="container">
+        				<h1 id="welcome">WELCOME TO CYBERNET INTERNET CAFE</h1>
+						<h2>We learn with you, we can't say we can't do it</h2>
+    				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+  <br><br>
+
+  
+  <div id="row">
+		 <div class="services">
+			 <h1 id="serve">Services</h1>
+			 <h3 id="design">
+				 <ul>
+					 <li>Business cards print-outs and Designs</li>
+					 <li><span style="color:black;">software Installations & repairs</span></li>
+					 <li>Email . Scan . Copy . Laminating</li>
+					 <li><span style="color:black;">Create Business google profiles</span></li>
+					 <li>printing & CV Typing</li>
+					 <li><span style="color:black;">Online Registrations</span></li>
+					 <li>Website Design</li>
+					 <li><span style="color:black;">movie downloads</span></li>
+				</ul>
+		</div>
+
+		<div class="aboutUs" >
+			
+			<!--<img src="images/img10.jpg" width = "100%" height = "250px">
+			<hr>-->
+			
+				<h1>About Us</h1>
+				<h3>
+				One of the areas where Cyber Net Internet Cafe excels
+				 from her competitors is the commitment to quality service.
+				 We understand that sometimes one of our customers might feel
+				 like the advocated quality service does not equate to the service they received.
+				 Please we would love to know about it. 
+				 Please we ask you to hold us accountable to any kind of incertisifaction.
+				Get in touch with Cyber Net Internet Cafe Management</h3>
+ 			
+        </div>
+</div>
+  <br><br>
+<div class="portfolio" style="width:100%">
+<br> 
+	<div id="img">
+		<div >
+			<img src="images/img4.jpeg" width="100%" height="200px" style="border-radius:12px;">
+       </div>
+	   &nbsp
+	   <div >
+			<img src="images/img3.jpeg" width="100%" height="200px" style="border-radius:12px;">
+       </div>
+	   &nbsp
+	   <div >
+			<img src="images/img2.jpeg" width="100%"  height="200px" style="border-radius:12px;">
+       </div>
+	   &nbsp
+	   <div >
+			<img src="images/img1.jpeg" width="100%"  height="200px" style="border-radius:12px;">
+       </div>
+	   &nbsp
+	   <div >
+			<img src="images/img4.jpeg" width="100%"  height="200px" style="border-radius:12px;">
+       </div>
+	  
+   </div> 
+
+</div>
+
+</body>
+
+<br><br><br><br>
+
+<footer id="fot">
+  
+  <div id="row">
+	  <div class="col">
+		  <h2>Address</h2>
+		  <p>19668 Tsamaya Road</p>
+		  <p>Mamelodi East</p>
+		  <p>0122</p>
+      </div>
+
+      <div class="col">
+		  <h2>Contact Info</h2>
+		  <p>Phone: 073 166 4529</p>
+		  <p>Alternative: 076 459 8774</p>
+		  <p>Email: <a href="mailto:cybernetinternetcafe555@gmail.com">cybernetinternetcafe555@gmail.com</a></p>
+      </div>
+
+	  <div class="col">
+		  <h2>Connect with us</h2>
+		  <p><a href="">Facebook</a></p>
+		  <p><a href="">twitter</a></p>
+		
+      </div>
+	  
+
+  </div>
+      <hr class="fotterP">
+      <p class="fotterP">Cybernet © <span style="float:right;">Copyright 2021 - All Rights Reserved</span></p>
+
+
+</footer>
+
+
+</html>
