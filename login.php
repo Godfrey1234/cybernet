22694b58d51a1ae6c08cbbeeddd862109c216689
Godfrey1234 <godfrey555mabena@gmail.com>
2022-07-06 13:17:45 +0200

    updates

---
+++ login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<link  rel="stylesheet" type= "text/css" href="style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
+<head>
+    
+ </head>
+
+
+  <body>
+
+   <div id="content" style="height:500px;">
+
+     <div id = "row">
+        
+        
+      
+        <div id ="loginImg">
+            <img src="images/myhome.png" style="height:680px">
+        </div>
+
+
+        <div id ="loginForm"  style="height:680px">
+       
+         <br><br>
+         <form  method="post">
+		  
+         <a href ="index.php"><i id="homeIcon" style ="margin-left:5%; color:white; font-size:2em; "class="fa fa-home"></i></a> 
+          <h3 style ="font-size:2em; color:white;">Welcome Back</h3>
+          
+          <input  style = "padding-top:10px; padding-bottom:10px; border-radius:15px" class= "form-control" placeholder="Email" type="text" name="username" required><br>
+		   
+		   
+          <br>
+          <input style = "padding-top:10px; padding-bottom:10px; border-radius:15px" class= "form-control" placeholder="Password" type="password" name="password" required><br>
+		  
+          <div id = "row">
+          <p style="text-align:left; margin-left:2%;padding-left:10%; "><a href="Registration.php" style = "color:white;">Create Account?</a></p>
+           <p style="text-align:right; margin-right:2%; padding-left:15%;"><a href="forgotpassword.php" style = "color:white;">Forgot Password?</a></p>
+          </div>
+		   <br>
+           &nbsp;<button id="btnaccount" type="submit" name="login" >Login</button> 
+		   <br>
+          <hr>
+		  <div><br></div>
+          <div>
+          
+	      
+          
+         </div>
+          
+        
+
+         </form >
+            
+        </div>
+
+     </div>
+   </div>
+
+
+
+  </body>
+ 
+  
+
+</html>       
