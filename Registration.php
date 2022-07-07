0c02ee78448328689f225f2d8e61e642186d61b7
Godfrey1234 <godfrey555mabena@gmail.com>
2022-07-07 17:51:06 +0200

    updates

---
+++ Registration.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+ <link  rel="stylesheet" type= "text/css" href="css/styles.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
+ 
+ </head>
+
+
+  <body>
+
+   <div id="content" style="height:500px;">
+
+     <div id = "row">
+        
+        
+      
+        <div id ="loginImg">
+            <img src="images/myhome.png" style="height:680px">
+        </div>
+
+
+        <div id ="loginForm"  style="height:auto; size:2em; ">
+
+         <br><br>
+         <form  method="post">
+		  
+		 <a href ="index.php"><i id="homeIcon" style ="margin-left:5%; color:white; font-size:2em; "class="fa fa-home"></i></a> 
+
+          <h3 style ="font-size:2em; color:white;">Register With us</h3>
+          
+          <input  style = "padding-top:10px; padding-bottom:10px; border-radius:15px" class= "form-control" placeholder="Name" type="text" name="name" required><br>
+		   
+		   
+          <br>
+          <input style = "padding-top:10px; padding-bottom:10px; border-radius:15px" class= "form-control" placeholder="Surname" type="text" name="surname" required><br>
+
+		  <br>
+          <input style = "padding-top:10px; padding-bottom:10px; border-radius:15px" class= "form-control" placeholder="Email" type="email" name="email" required><br>
+		  
+		  <br>
+          <input style = "padding-top:10px; padding-bottom:10px; border-radius:15px" class= "form-control" placeholder="Contact Number" type="tel" name="contact" required><br>
+		  
+		  <br>
+          <input style = "padding-top:10px; padding-bottom:10px; border-radius:15px" class= "form-control" placeholder="Password" type="password" name="password" required><br>
+		  
+		  <br>
+          <input style = "padding-top:10px; padding-bottom:10px; border-radius:15px" class= "form-control" placeholder="Confirm password" type="tel" name="Confirm" required><br>
+		  
+		  
+          <div id = "row">
+         
+           <p style="text-align:right; margin-right:1%; padding-left:25%;">Already have an account? <a href="login.php" style = "color:white;"><u>Login</u></a></p>
+          </div>
+		   <br>
+           &nbsp;<button id="btnaccount" type="submit" name="Register" >Register</button> 
+		   <br>
+          <hr>
+		  <div><br></div>
+          <div>
+          
+	      
+          
+         </div>
+          
+        
+
+         </form >
+            
+        </div>
+
+     </div>
+   </div>
+
+
+
+  </body>
+ 
+  
+
+</html>       
---
+++ home.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <link  rel="stylesheet" type= "text/css" href="css/styles.css">	
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+  </head>
+
+<body>
+
+<nav>
+		<img src="images/bgP.png" id="logo">
+		<ul>
+			<li><a href="#welcome">Home</a></li>
+			<li><a href="https://www.google.com/maps/dir/-25.7050285,28.3812558/cyberNet+Internet+cafe+Pretoria/@-25.706065,28.3817844,17z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x1e955f88e4e4d271:0x9c93bea38b74354a!2m2!1d28.3865657!2d-25.7072593">Directions</a></li>
+			<li><a href="#fot">Contact Us</a></li>
+			<li><a href="index.php">Logout</a></li>
+       </ul>
+ </nav>
+
+
+ <div id = "OurServices" > 
+ 
+	
+
+    <br>
+
+   <h3 style = "font-size:2em; padding-top:1%;">Online Services </h3>
+
+    <div id = "row">
+     
+	 <div id ="service1">
+		<Img id="cvImg"src="images/cv4.jpg"></Img>
+		<h3 id = "desc">Create CV <h3/>
+     </div>
+
+	 <div id ="service1">
+		<Img id="cvImg"src="images/qt3.png"></Img>
+		<h3 id = "desc">Create Quotation <h3/>
+     </div>
+
+	 <div id ="service1">
+		<Img id="cvImg"src="images/scan2.jpg"></Img>
+		<h3 id = "desc">Scan Documents <h3/>
+     </div>
+
+	 <div id ="service1">
+		<Img id="cvImg"src="images/lt1.png"></Img>
+		<h3 id = "desc">Write a Letter<h3/>
+     </div>
+
+	 <div id ="service1">
+		<Img id="cvImg"src="images/jobs1.jpg"></Img>
+		<h3 id = "desc">View Online Jobs<h3/>
+     </div>
+	 
+ 
+    </div>
+
+
+    
+	<div id = "row">
+     
+	 <div id ="service1">
+		<Img id="cvImg"src="images/cp1.jpg"></Img>
+		<h3 id = "desc">Company Registration<h3/>
+     </div>
+
+	 <div id ="service1">
+		<Img id="cvImg"src="images/logoD1.png"></Img>
+		<h3 id = "desc">Request For logo Design<h3/>
+     </div>
+
+	 <div id ="service1">
+		<Img id="cvImg"src="images/contract2.jpg"></Img>
+		<h3 id = "desc">Request for Contract<h3/>
+     </div>
+
+	 <div id ="service1">
+		<Img id="cvImg"src="images/web1.jpg"></Img>
+		<h3 id = "desc">Web Design & Hosting<h3/>
+     </div>
+
+	 <div id ="service1">
+		<Img id="cvImg"src="images/uni1.jpg"></Img>
+		<h3 id = "desc">See Which University You Qualify for<h3/>
+     </div>
+	 
+ 
+    </div>
+ 
+    <br><br>
+ </div>
+
+
+<br>
+ <div id ="content">
+  <div id= "videoGuide" style="margin-top:0px;" >
+  
+  <video width="60%" height="70%" controls>
+  <source src="images/explain.mp4" type="video/mp4">
+  <source src="movie.ogg" type="video/ogg">
+    Your browser does not support the video tag.
+
+  </video>
+  <h2 id = "cyberTextH">Watch Our Video To Learn About Our Online Service</h2>
+</div>
+  
+
+ </div>
+  <br>
+
+  <br>
+<div class="portfolio" style="width:100%">
+<br> 
+	<div id="img">
+		<div >
+			<img src="images/img4.jpeg" width="100%" height="200px" style="border-radius:12px;">
+       </div>
+	   &nbsp
+	   <div >
+			<img src="images/img3.jpeg" width="100%" height="200px" style="border-radius:12px;">
+       </div>
+	   &nbsp
+	   <div >
+			<img src="images/img2.jpeg" width="100%"  height="200px" style="border-radius:12px;">
+       </div>
+	   &nbsp
+	   <div >
+			<img src="images/img1.jpeg" width="100%"  height="200px" style="border-radius:12px;">
+       </div>
+	   &nbsp
+	   <div >
+			<img src="images/img4.jpeg" width="100%"  height="200px" style="border-radius:12px;">
+       </div>
+	  
+   </div> 
+
+</div>
+
+</body>
+
+<br><br><br><br>
+
+<footer id="fot">
+  
+  <div id="row">
+	  <div class="col">
+		  <h2>Address</h2>
+		  <p>19668 Tsamaya Road</p>
+		  <p>Mamelodi East</p>
+		  <p>0122</p>
+      </div>
+
+      <div class="col">
+		  <h2>Contact Info</h2>
+		  <p>Phone: 073 166 4529</p>
+		  <p>Alternative: 076 459 8774</p>
+		  <p>Email: <a href="mailto:cybernetinternetcafe555@gmail.com">cybernetinternetcafe555@gmail.com</a></p>
+      </div>
+
+	  <div class="col">
+		  <h2>Connect with us</h2>
+		  <p><a href="">Facebook</a></p>
+		  <p><a href="">twitter</a></p>
+		
+      </div>
+	  
+
+  </div>
+      <hr class="fotterP">
+      <p class="fotterP"><a href ="admin.php">Cybernet © </a><span style="float:right;">Copyright 2021 - All Rights Reserved</span></p>
+
+
+</footer>
+
+
+</html>
