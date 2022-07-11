0d71c3ef0b1d1494897e0d70b002e69e21d6e613
Godfrey1234 <godfrey555mabena@gmail.com>
2022-07-11 11:24:25 +0200

    updates

--- login.php
+++ login.php
@@ -8,10 +8,52 @@
  </head>
 
 
-  <body id="body2" style="background-image:url(images/myhome.png); background-size: cover;height:100%; margin-top:0%; margin-bottom:0%; padding-top:0%; padding-bottom:0%;" >
+  <body style="height:auto; margin-top:0%; margin-bottom:0%; background-color:darkslategray;" >
+
+   <div id="content5" style ="background: url('images/myhome.png');background-size: cover; background-repeat: no-repeat; width:70%; margin-top:0%; margin-bottom:0%;" >
+
+        <div id ="loginForm"  style="height:680px; width:60%; margin-left:70%; padding-top:5%; margin-top:0%; margin-bottom:0%;g " >
+       
+         <br><br>
+         <form  method="post">
+		  
+         <a href ="index.php"><i id="homeIcon" style ="margin-left:5%; color:white; font-size:2em; "class="fa fa-home"></i></a> 
+          <h3 style ="font-size:2em; color:white;">Welcome Back</h3>
+          
+          <input  style = "padding-top:10px; padding-bottom:10px; border-radius:15px" class= "form-control" placeholder="Email" type="text" name="username" required><br>
+		   
+		   
+          <br>
+          <input style = "padding-top:10px; padding-bottom:10px; border-radius:15px" class= "form-control" placeholder="Password" type="password" name="password" required><br>
+		  
+          <div id = "row">
+          <p style="text-align:left; margin-left:2%;padding-left:10%; "><a href="Registration.php" style = "color:white;">Create Account?</a></p>
+           <p style="text-align:right; margin-right:2%; padding-left:15%;"><a href="forgotpassword.php" style = "color:white;">Forgot Password?</a></p>
+          </div>
+		   <br>
+           &nbsp;<button id="btnaccount" type="submit" name="login" >Login</button> 
+		   <br>
+          <hr>
+		  <div><br></div>
+          <div>
+          
+	      
+          
+         </div>
+          
+        
+
+         </form >
+            
+        </div>
+
+     </div>
+  
+
 
 
   </body>
  
+  
 
 </html>       
